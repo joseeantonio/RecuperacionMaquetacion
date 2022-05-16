e166a06ce7ed92d046e8c735b5c96fa8e4c1869f
Jose antonio <jagonzalez.alumno@iesrafaelalberti.es>
2022-05-16 16:46:22 +0200

    reorganizacion

--- dist/index.0f645504.js
+++
@@ -1,2 +0,0 @@
-"use strict";document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".menu_hamburguesa"),t=document.querySelector(".menu_movil");e.addEventListener("click",(function(){t.classList.toggle("mostrar")}))}));
-//# sourceMappingURL=index.0f645504.js.map
