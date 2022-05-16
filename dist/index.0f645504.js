b2888e2496128f761dd00ee5a0557be1710e4f12
Jose antonio <jagonzalez.alumno@iesrafaelalberti.es>
2022-05-16 16:42:12 +0200

    prueba para github page

---
+++ dist/index.0f645504.js
@@ -0,0 +1,2 @@
+"use strict";document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".menu_hamburguesa"),t=document.querySelector(".menu_movil");e.addEventListener("click",(function(){t.classList.toggle("mostrar")}))}));
+//# sourceMappingURL=index.0f645504.js.map
